cb41438a5f16fd657c510eae0f32a7d18ec71a6e
Max Novelli <max.novelli@ess.eu>
2026-06-03 13:35:32 +0200

    ranamed util to utils, added readme

--- utils/create_single_document.bash
+++ utils/create_single_document.bash
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
 cat > cover.md <<EOF
 ---
 title: "SciCat Project Governance"
@@ -22,18 +26,23 @@ rm -f combined.md
 while read file; do
     sed '/^\*\*\*$/,/^Licensed/d' "$file" >> combined.md
     echo -e "\n\n\\newpage\n" >> combined.md
-done < manifest.txt > combined.md
+done < manifest.txt
+
+# Copy combined.md to final markdown output
+cp combined.md "scicat_governance_full_${TIMESTAMP}.md"
 
 # convert document to pdf
 pandoc \
   cover.md \
   combined.md \
-  -o scicat_governance_full_20260602.pdf \
+  -o "scicat_governance_full_${TIMESTAMP}.pdf" \
   --toc \
   --pdf-engine=xelatex \
   -V geometry:"a4paper,portrait,margin=1.5cm" \
   -V fontsize=6pt
 
-# fix links in comdine markdown
-sed -Ei 's|\((\./)?[^)]*/([A-Z-]+)\.md\)|(#\L\2)|g' combined.md
+# fix links in combined markdown
+sed -Ei 's|\((\./)?[^)]*/([A-Z-]+)\.md\)|(#\L\2)|g' "scicat_governance_full_${TIMESTAMP}.md"
 
+# remove temporary files
+rm -f cover.md combined.md
